2aa15cef6e0775c2308cc368cb823c3d4089ea52
Nataliia <nataliiaqa8@gmail.com>
2024-08-07 16:46:10 +0200

    ;liusdg

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,44 +1,52 @@
 const formInfo = document.querySelector('.feedback-form');
-let formData = {};
+let formData = {
+    email: "",
+    message: ""
+};
 
+const fillFormFields = () => {
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-const formFileSubmit = event =>{
-    event.preventDefault();
-    event.target.reset();
-    if (!formData.email || !formData.message) {
-        alert('Fill please all fields');
+    if (formDataFromLS === null) {
         return;
     }
 
-    localStorage.removeItem('feedback-form-state');
+    formData = formDataFromLS;
 
-}
-
-const fillFormFields = () =>{
-const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-if(formDataFromLS === null){
-return;
-}
-
-formData = formDataFromLS;
-
-
-for(const key in formDataFromLS){
-    if(formDataFromLS.hasOwnProperty(key)){
-        formInfo.elements[key].value = formDataFromLS[key];
+    for (const key in formDataFromLS) {
+        if (formDataFromLS.hasOwnProperty(key)) {
+            formInfo.elements[key].value = formDataFromLS[key];
+        }
     }
-}
-}
+};
 
 fillFormFields();
 
 const formFileInput = event => {
     const formValue = event.target.value;
     const formName = event.target.name;
-formData[formName] = formValue;   
-const formDataLS = localStorage.setItem('feedback-form-state' , JSON.stringify(formData));
-}
+    formData[formName] = formValue;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const formFileSubmit = event => {
+    event.preventDefault();
+
+    if (!formInfo.elements.email.value.trim() || !formInfo.elements.message.value.trim()) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem('feedback-form-state');
+    formData = {
+        email: "",
+        message: ""
+    };
+
+    event.target.reset();
+};
 
 formInfo.addEventListener('input', formFileInput);
 formInfo.addEventListener('submit', formFileSubmit);
